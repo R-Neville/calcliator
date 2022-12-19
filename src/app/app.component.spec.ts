2dd8caed267e763d8d41b3603a105cf877bd99b9
Robert Neville <robertneville27@gmail.com>
2022-12-19 14:00:55 +1000

    More renaming actions

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'calucator'`, () => {
+  it(`should have as title 'calcliator'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('calucator');
+    expect(app.title).toEqual('calcliator');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('calucator app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('calcliator app is running!');
   });
 });
