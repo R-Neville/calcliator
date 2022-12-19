449a86b262896c26245fc3ee6634bd36407040a3
Robert Neville <robertneville27@gmail.com>
2022-12-19 11:26:31 +1000

    Generate calculator component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { CoreModule } from './core/core.module';
 import { AppComponent } from './app.component';
 import { AboutPageComponent } from './about-page/about-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { CalculatorComponent } from './home-page/calculator/calculator.component';
 
 @NgModule({
-  declarations: [AppComponent, AboutPageComponent, HomePageComponent],
+  declarations: [AppComponent, AboutPageComponent, HomePageComponent, CalculatorComponent],
   imports: [BrowserModule, AppRoutingModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
